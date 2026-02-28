dd0244898a9ee9940be15dc4ffd1eaf83abf731a
shaneriob-bit <shaneriob@gmail.com>
2026-02-28 16:25:20 -0500

    Implement Geek Bar Speed Boost

--- scripts/collectible/geek_bar.gd
+++ scripts/collectible/geek_bar.gd
@@ -1,4 +1,14 @@
 extends Node3D
 class_name GeekBar
 
-var player
+var player : Player
+
+func on_consume():
+	if player:
+		player.create_speed_modifier(10.0, 10.0)
+
+func _on_body_entered(body: Node) -> void:
+	if body is Player:
+		player = body
+		on_consume()
+		queue_free()
