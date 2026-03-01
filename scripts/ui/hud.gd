f3f1f4b3b526f4b2d99b89ced74fab1a7f4fd7c7
shaneriob-bit <shaneriob@gmail.com>
2026-03-01 15:46:54 -0500

    use msaa

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,5 +1,5 @@
 extends Control
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	$TextureProgressBar.value = RoundManager.geek_value
